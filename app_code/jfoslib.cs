7a354e631e1c30de570b28ebd9063cb69f3c8efa
maehara <maehara@jfos.co.jp>
2021-01-27 10:38:13 +0900

    comit

---
+++ app_code/jfoslib.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Web.UI;
+using System.Data;
+using System.Text;
+using System.Security.Cryptography;
+using Npgsql;
+
+namespace JfosLib
+{
+   public class DB
+   {
+      /* postgre 接続文字列設定用 */
+      private string server =    "localhost";
+      private string port =      "5433";
+      private string user_id =   "postgres";
+      private string database =  "TSKWEBDB01";
+      private string password =  "jfos";
+      private string enlist =    "true";
+      
+      public string constr;
+      public NpgsqlConnection conn;
+      public NpgsqlCommand cmd;
+      public NpgsqlDataAdapter adp;
+      
+      public DB(string SQL)
+      {
+         this.constr = "Server=" + server + ";Port=" + port + ";User ID=" + user_id + ";Password=" + password + ";Database=" + database + ";Enlist=" + enlist + ";";
+         this.conn = new NpgsqlConnection(this.constr);
+         this.cmd = new NpgsqlCommand();
+         this.cmd.Connection = this.conn;
+         this.cmd.CommandText = SQL;
+         this.adp = new NpgsqlDataAdapter(this.cmd);
+      }
+   }
+   
+   public class Hash
+   {
+      public string str = "";
+      
+      public Hash(string txt)
+      {
+         byte[] data = Encoding.UTF8.GetBytes(txt);
+         var md5 = MD5.Create();
+         byte[] bs = md5.ComputeHash(data);
+
+         //md5のリソース解放//
+         md5.Clear();
+         
+         var hexa = new StringBuilder();
+         
+         foreach(var b in bs)
+         {
+            hexa.Append(b.ToString("x2"));
+         }
+         this.str = hexa.ToString();
+      }
+   }
+}
